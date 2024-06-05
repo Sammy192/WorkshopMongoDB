709f9ea677e81c613c93fcca6b00cb1215ebfd8f
Samuel R Borges <sammy_rb@hotmail.com>
2024-06-05 17:14:53 -0300

    update user created

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -23,9 +23,13 @@ public class UserService {
     }
 
     public UserDTO findById(String id) {
-        Optional<User> result = repository.findById(id);
-        User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+       User entity = getEntityById(id);
         return new UserDTO(entity);
+    }
+
+    private User getEntityById(String id) {
+        Optional<User> result = repository.findById(id);
+        return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
     }
 
     public UserDTO insert(UserDTO dto) {
@@ -33,6 +37,13 @@ public class UserService {
         copyDtoToEntity(dto, entity);
         entity = repository.insert(entity);
         return new UserDTO(entity);
+    }
+
+    public UserDTO update(String id, UserDTO dto) {
+        User entity = getEntityById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return  new UserDTO(entity);
     }
 
     private void copyDtoToEntity(UserDTO dto, User entity) {
