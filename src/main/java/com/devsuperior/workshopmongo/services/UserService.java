9c37a73e3146ea266013bdacfe5d103c3f1633d7
Samuel R Borges <sammy_rb@hotmail.com>
2024-06-05 12:36:51 -0300

    get all users

---
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.workshopmongo.services;
+
+import com.devsuperior.workshopmongo.models.dto.UserDTO;
+import com.devsuperior.workshopmongo.models.entities.User;
+import com.devsuperior.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<UserDTO> findAll() {
+        List<User> list = repository.findAll();
+        return list.stream().map(x -> new UserDTO()).collect(Collectors.toList());
+    }
+}
