7a69cfc77873ead7d608843b3eabbb1e82a164c7
Samuel R Borges <sammy_rb@hotmail.com>
2024-06-06 11:23:21 -0300

    GET Post by title, usando query methods

--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -30,4 +30,9 @@ public class PostService {
         return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
     }
 
+    public List<PostDTO> findByTitle(String text) {
+        List<Post> list = repository.findByTitleContainingIgnoreCase(text);
+        return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+    }
+
 }
