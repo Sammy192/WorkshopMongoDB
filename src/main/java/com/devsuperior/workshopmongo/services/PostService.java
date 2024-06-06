43c0359db46bac90e71f2f2c414460d7b8558a2e
Samuel R Borges <sammy_rb@hotmail.com>
2024-06-06 12:34:38 -0300

    GET Post full search

--- src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -10,6 +10,8 @@ import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundExcepti
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -34,6 +36,24 @@ public class PostService {
         List<Post> list = repository.searchTitle(text);
         //List<Post> list = repository.findByTitleContainingIgnoreCase(text);
         return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+    }
+
+    public List<PostDTO> fullSearch(String text, String start, String end) {
+        Instant startMoment = convertMoment(start, Instant.ofEpochMilli(0L));
+        Instant endMoment = convertMoment(end, Instant.now());
+
+        List<Post> list = repository.fullSearch(text, startMoment, endMoment);
+
+        return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+    }
+
+    private Instant convertMoment(String originalString, Instant alternative) {
+        try {
+            return Instant.parse(originalString);
+        }
+        catch (DateTimeParseException e ) {
+            return  alternative;
+        }
     }
 
 }
